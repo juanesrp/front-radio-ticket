417f33694e5376cde6e8801cb76d3464d4ffb6ae
juanesrp <juanesrp@gmail.com>
2024-05-28 11:23:51 -0500

    Se agregan la ruta events y su ruta diinamica a el detalle de eventos y se le agrega la fecha a el eventPreLoad, tambien agregue paginacion

--- src/components/Events/CardEvents.tsx
+++ src/components/Events/CardEvents.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const CardEvents = (event: IEvent) => {
   return (
     <div className="col-span-1 relative flex flex-col items-center bg-gray-100 hover:bg-gray-200">
-      <Link href={`/events/${event.id}`} className="p-6">
+      <Link href={`/Events/${event.id}`} className="p-6">
         <div className="flex justify-center">
           <img src={event.image} className="w-40" />
         </div>
@@ -13,7 +13,9 @@ const CardEvents = (event: IEvent) => {
           <h1 className="text-lg font-bold text-center">
             {event.date + " | " + event.name}
           </h1>
-          <h2>{event.price}</h2>
+          <h2 className="text-md text-center font-medium">
+            Desde ${event.price}
+          </h2>
         </div>
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
           <span className="bg-red-600 text-white p-4 font-semibold">
