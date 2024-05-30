386eaa6f14d92dfdcdf693f6f66c18f2f9ef25c7
juanesrp <juanesrp@gmail.com>
2024-05-30 11:27:16 -0500

    Se traen los eventos desde el back y modifique interfaz IEvent

--- src/app/concerts/[id]/page.tsx
+++ src/app/concerts/[id]/page.tsx
@@ -1,9 +1,25 @@
+"use client";
 import CardEventDetail from "@/components/Events/EventDetail/CardEventDetail";
 import { eventPreLoad } from "@/helpers/eventPreLoad";
-import React from "react";
+import { IEvent } from "@/interfaces";
+import { getEventById } from "@/utils/events.util";
+import React, { useEffect, useState } from "react";
 
-const page = ({ params }: { params: { id: number } }) => {
-  const event = eventPreLoad.find((event) => event.id === Number(params.id));
+const page = ({ params }: { params: { id: string } }) => {
+  const [event, setEvent] = useState<IEvent>();
+  const fetchEvents = async () => {
+    try {
+      const event: IEvent = await getEventById(params.id);
+      setEvent(event);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
     <div className="bg-gray-50">
       {event ? <CardEventDetail event={event} /> : "No se encontro el evento"}
