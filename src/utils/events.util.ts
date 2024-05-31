a1ef4c56f08574bbd9e672f1b4d3174f5889d9d1
LuisRGN <luisrafael1598@gmail.com>
2024-05-30 19:45:20 -0500

    se modifica la peticion alback para traer el token que persiste en el localStorage para poder hacer un post siendo admin o super admin, se acomoda el formulario y se trabajan las alertas para la confirmacion y los errores se ajusta el boton del admin y se cambia el color del loading

--- src/utils/events.util.ts
+++ src/utils/events.util.ts
@@ -1,62 +1,76 @@
 import axios from "axios";
 import { IEvent } from "@/interfaces";
 const api = process.env.NEXT_PUBLIC_API;
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE0Mjk4MzczLTM2NzQtNDA4MS1hZjUwLTI4ZmI5ODM2YTc3ZiIsImVtYWlsIjoieDhsOGNAZXhhbXBsZS5jb20iLCJpc0FkbWluIjpmYWxzZSwiaXNTdXBlckFkbWluIjp0cnVlLCJpYXQiOjE3MTcwNzk5NTQsImV4cCI6MTcxNzA4MzU1NH0.WdPoY7VWIe-hHaeQSnxFpIbsw-j_VMlFrUl9Ct23i5I";
 
 export const getEvents = async (
-  page: number,
-  limit: number
+    page: number,
+    limit: number
 ): Promise<IEvent[]> => {
-  try {
-    const res = await axios.get(`${api}/events`, {
-      params: {
-        page,
-        limit,
-      },
-    });
-    const events: IEvent[] = res.data.events;
-    console.log("Estoy en el getEvents: ", events);
-    return events;
-  } catch (error: any) {
-    throw new Error("Ocurrio un error: ", error);
-  }
+    try {
+        const res = await axios.get(`${api}/events`, {
+            params: {
+                page,
+                limit,
+            },
+        });
+        const events: IEvent[] = res.data.events;
+        console.log("Estoy en el getEvents: ", events);
+        return events;
+    } catch (error: any) {
+        throw new Error("Ocurrio un error: ", error);
+    }
 };
 
 export const getEventById = async (id: string) => {
-  try {
-    const res = await fetch(`${api}/events/${id}`);
-    const event: IEvent = await res.json();
-    return event;
-  } catch (error: any) {
-    throw new Error(error);
-  }
+    try {
+        const res = await fetch(`${api}/events/${id}`);
+        const event: IEvent = await res.json();
+        return event;
+    } catch (error: any) {
+        throw new Error(error);
+    }
 };
 
 export const postEvent = async (eventData: {
-  name: string;
-  description: string;
-  imgUrl: string;
-  category: string;
-  date: string;
-  location: string;
-  tickets: {
-    price: number;
-    stock: number;
-    zone: string;
-  }[];
+    name: string;
+    description: string;
+    imgUrl: string;
+    category: string;
+    date: string;
+    location: string;
+    tickets: {
+        price: number;
+        stock: number;
+        zone: string;
+    }[];
 }) => {
-  try {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer ${token}`,
-      },
-    };
+    try {
+        const userSesion = localStorage.getItem('userSession')
+        let token: string | null = null
 
-    const res = await axios.post(`${api}/events`, eventData, config);
-    return res.data;
-  } catch (error: any) {
-    return { error };
-  }
-};
+        if (userSesion) {
+            const userSesionToken = JSON.parse(userSesion)
+            token = userSesionToken ? userSesionToken.token : null;
+        } else {
+            return { error: "No se encontró userSesion en el localStorage" }
+        }
+
+        if (!token) {
+            return { error: "Token no encontrado en userSesion" }
+        }
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        const res = await axios.post(`${api}/events`, eventData, config);
+        return res.data;
+    } catch (error: any) {
+        if (axios.isAxiosError(error) && error.response) {
+            return { error: error.response.data.message };
+        }
+        return { error: "Error desconocido al crear el evento" };
+    }
+}
